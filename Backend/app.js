5bdcc42f8b4d364d521572202fb4641e76857452
Rupala Rut <rutrupala.ce@gmail.com>
2025-09-06 09:25:58 +0530

    Dynamic Import

---
+++ Backend/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { readdirSync } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+app.use(express.json());
+
+// Fix __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const routesPath = path.join(__dirname, "routes");
+const routeFiles = readdirSync(routesPath).filter(f => f.endsWith("Routes.js"));
+
+for (const file of routeFiles) {
+  const { default: route } = await import(`./routes/${file}`);
+  const routeName = file.replace("Routes.js", "").toLowerCase();
+  app.use(`/api/${routeName}`, route);
+}
+
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
